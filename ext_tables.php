8f453fe736af303d48f102b2798793834f813886
ianbogda <ianbogda@gmail.com>
2015-05-20 15:50:25 +0200

    * globla to tt_content

--- ext_tables.php
+++ ext_tables.php
@@ -10,38 +10,36 @@ $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = unserialize($GLOBALS['
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'pradines');
 
-$TCA['pages']['columns'] += array(
-    'icon' => array(
-        'label' => 'Pradines',
-        'exclude' => 1,
-        'config' => array(
-            'type' => 'select',
-            'selicon_cols' => 14,
-            'items' => array(
-                array('Aucun', 0, 'EXT:pradines/Resources/Public/Images/Icons/none.jpg'),
-			array('culture', 'culture', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/culture.png'),
-			array('union', 'union', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/union.png'),
-			array('recyclage', 'recyclage', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/recyclage.png'),
-			array('info', 'info', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/info.png'),
-			array('annonce', 'annonce', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/annonce.png'),
-			array('rameau', 'rameau', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/rameau.png'),
-			array('contact', 'contact', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/contact.png'),
-			array('sante', 'sante', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/sante.png'),
-			array('rencontre', 'rencontre', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/rencontre.png'),
-			array('transport', 'transport', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/transport.png'),
-			array('naissance', 'naissance', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/naissance.png'),
-			array('animation', 'animation', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/animation.png'),
-			array('people', 'people', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/people.png'),
-			array('flamme', 'flamme', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/flamme.png'),
-			array('individu', 'individu', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/individu.png'),
-			array('finance', 'finance', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/finance.png'),
-			array('mairie', 'mairie', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/mairie.png'),
+$tca = array(
+    'columns' => array(
+        'icon' => array(
+            'label' => 'Pradines',
+            'config' => array(
+                'type' => 'select',
+                'selicon_cols' => 14,
+                'items' => array(
+                    array('Aucun', 0, 'EXT:pradines/Resources/Public/Images/Icons/none.jpg'),
+                    array('culture', 'culture', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/culture.png'),
+                    array('union', 'union', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/union.png'),
+                    array('recyclage', 'recyclage', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/recyclage.png'),
+                    array('info', 'info', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/info.png'),
+                    array('annonce', 'annonce', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/annonce.png'),
+                    array('rameau', 'rameau', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/rameau.png'),
+                    array('contact', 'contact', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/contact.png'),
+                    array('sante', 'sante', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/sante.png'),
+                    array('rencontre', 'rencontre', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/rencontre.png'),
+                    array('transport', 'transport', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/transport.png'),
+                    array('naissance', 'naissance', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/naissance.png'),
+                    array('animation', 'animation', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/animation.png'),
+                    array('people', 'people', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/people.png'),
+                    array('flamme', 'flamme', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/flamme.png'),
+                    array('individu', 'individu', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/individu.png'),
+                    array('finance', 'finance', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/finance.png'),
+                    array('mairie', 'mairie', 'EXT:pradines/Resources/Public/Images/Icons/pradines/png/mairie.png'),
+            ),
             ),
         ),
     ),
 );
 
-t3lib_extMgm::addToAllTCATypes (
-    'pages',
-    'icon'
-);
+['TCA']['tt_content'] = array_replace_recursive(['TCA']['tt_content'], );
--- generateExtTables_dot_php.sh
+++ generateExtTables_dot_php.sh
@@ -15,30 +15,28 @@ if (!is_array(\$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][\$_EXTKEY])) {
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(\$_EXTKEY, 'Configuration/TypoScript/', 'pradines');
 
-\$TCA['pages']['columns'] += array(
-    'icon' => array(
-        'label' => 'Pradines',
-        'exclude' => 1,
-        'config' => array(
-            'type' => 'select',
-            'selicon_cols' => 14,
-            'items' => array(
-                array('Aucun', 0, 'EXT:pradines/Resources/Public/Images/Icons/none.jpg')," > ext_tables.php
+\$tca = array(
+    'columns' => array(
+        'icon' => array(
+            'label' => 'Pradines',
+            'config' => array(
+                'type' => 'select',
+                'selicon_cols' => 14,
+                'items' => array(
+                    array('Aucun', 0, 'EXT:pradines/Resources/Public/Images/Icons/none.jpg')," > ext_tables.php
 
 find . -iname '*.png' -exec echo "{}" \; | cut -d'/' -f 2,3,4,5,6,7,8 > list.txt
 for i in $(cat list.txt); do
     GLYPHICON=`echo $i | cut -d'/' -f 7 | cut -d'.' -f 1`
     if [[ ! "$i" =~ "Resources/Private" ]]; then
-        echo "			array('$GLYPHICON', '$GLYPHICON', 'EXT:pradines/$i')," >> ext_tables.php
+        echo "                    array('$GLYPHICON', '$GLYPHICON', 'EXT:pradines/$i')," >> ext_tables.php
     fi
 done
 
 echo "            ),
+            ),
         ),
     ),
 );
 
-t3lib_extMgm::addToAllTCATypes (
-    'pages',
-    'icon'
-);" >> ext_tables.php
+$GLOBALS['TCA']['tt_content'] = array_replace_recursive($GLOBALS['TCA']['tt_content'], $tca);" >> ext_tables.php
